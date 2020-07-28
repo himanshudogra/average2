f30c3fbdfe4fb13b036c4ebfe626acc0b36b8fd0
himanshu <himanshudogra96@gmail.com>
2020-07-28 18:34:00 +0530

    Create main.go

---
+++ int/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func average(numbers ...int) int {
+	var multiply int = 1
+	for _, number := range numbers {
+		multiply *= number
+	}
+	return multiply
+}
+
+func main() {
+	var numbers []int
+	arguments := os.Args[1:]
+
+	for _, argument := range arguments {
+
+		number, err := strconv.Atoi(argument)
+		if err != nil {
+			log.Fatal(err)
+		}
+		numbers = append(numbers, number)
+	}
+	fmt.Printf("Multiply %d\n", average(numbers...))
+}
